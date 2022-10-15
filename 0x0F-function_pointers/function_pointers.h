4e048e97cc893304523ec7cfb095d0e088d9625e
Alexandra-dev-13 <malexandra13@yahoo.com>
2022-10-14 19:50:58 -0700

    0. What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,18 +1,16 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stddef.h>
 
 /**
  * int prototypes
  * void prototypes
 */
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif /* FUNCTION_POINTERS_H */
+#endif /* _FUNCTION_POINTERS_H_ */
